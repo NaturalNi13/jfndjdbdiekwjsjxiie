eecc82a1d345ff0fd3d906eefb68c055b32cdc25
NaturalNi13 <naturalidiot1234@gmail.com>
2024-12-17 18:27:29 -0600

    Create trystealingnow.lua

---
+++ trystealingnow.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/jfndjdbdiekwjsjxiie/refs/heads/main/1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/jfndjdbdiekwjsjxiie/refs/heads/main/2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/jfndjdbdiekwjsjxiie/refs/heads/main/3.lua"))()
